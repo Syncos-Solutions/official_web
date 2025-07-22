fc9ce5cb4c16328618a351c8c987c52a2c023ac3
GihanthaDeshabi <gihanthadeshabi.a@gmail.com>
2025-07-22 21:32:02 +0530

    push 006

--- app/page.tsx
+++ app/page.tsx
@@ -264,14 +264,23 @@ export default function LandingPage() {
               className="relative mx-auto max-w-5xl"
             >
               <div className="rounded-xl overflow-hidden shadow-2xl border border-border/40 bg-gradient-to-b from-background to-muted/20">
-                <Image
+              
+    <iframe
+      src="https://my.spline.design/genkubgreetingrobot-tmlK71aXlgHtkIeplp1JMJDH/"
+     width={1280}
+      height={720}
+      allowfullscreen
+    ></iframe>
+  
+
+                {/* <Image
                   src="https://cdn.dribbble.com/userupload/12302729/file/original-fa372845e394ee85bebe0389b9d86871.png?resize=1504x1128&vertical=center"
                   width={1280}
                   height={720}
                   alt="SaaSify dashboard"
                   className="w-full h-auto "
                   priority
-                />
+                /> */}
                 <div className="absolute inset-0 rounded-xl ring-1 ring-inset ring-black/10 dark:ring-white/10"></div>
               </div>
               <div className="absolute -bottom-6 -right-6 -z-10 h-[300px] w-[300px] rounded-full bg-gradient-to-br from-primary/30 to-secondary/30 blur-3xl opacity-70"></div>
